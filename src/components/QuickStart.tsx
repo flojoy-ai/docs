3a81a9ce21dd07fd0e3125928eb7e6fa45726585
JeffDotPng <zhangjeff2014@gmail.com>
2023-06-14 17:04:47 -0400

    Format

--- src/components/QuickStart.tsx
+++ src/components/QuickStart.tsx
@@ -5,15 +5,15 @@ import clsx from 'clsx';
 const QuickStart = () => {
 	return (
 		<div className={clsx(styles.quickstart)}>
-			<div
-				className="container text-left"
-				style={{padding: '50px',}}
-			>
+			<div className="container text-left" style={{ padding: '50px' }}>
 				<h3 className="hero__subtitle">QuickStart!</h3>
 				<ul className={styles.list}>
 					<li>
 						<span className={styles.bulletLarge}>&#x25CF;</span>
-						<a href="nodes/INSTRUMENTS/STEPPER_MOTOR/STEPPER_DRIVER_TIC/" className={styles.link}>
+						<a
+							href="nodes/INSTRUMENTS/STEPPER_MOTOR/STEPPER_DRIVER_TIC/"
+							className={styles.link}
+						>
 							<span>Robotics</span>
 						</a>
 						<p className={styles.paragraph}>Control a stepper motor</p>
@@ -29,14 +29,20 @@ const QuickStart = () => {
 					</li>
 					<li>
 						<span className={styles.bulletLarge}>&#x25CF;</span>
-						<a href="nodes/INSTRUMENTS/SERIAL/SERIAL_SINGLE_MEASUREMENT/" className={styles.link}>
+						<a
+							href="nodes/INSTRUMENTS/SERIAL/SERIAL_SINGLE_MEASUREMENT/"
+							className={styles.link}
+						>
 							<span>Arduino</span>
 						</a>
 						<p className={styles.paragraph}>Read temperature from an Arduino</p>
 					</li>
 					<li>
 						<span className={styles.bulletLarge}>&#x25CF;</span>
-						<a href="nodes/LOADERS/LOCAL_FILE_SYSTEM/OPEN_MATLAB/" className={styles.link}>
+						<a
+							href="nodes/LOADERS/LOCAL_FILE_SYSTEM/OPEN_MATLAB/"
+							className={styles.link}
+						>
 							<span> MATLAB</span>
 						</a>
 						<p className={styles.paragraph}>
