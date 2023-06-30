f9d3691982e7e84ab69eb2c342775e3f1a8350c6
Jack Parmer <jparmer09@gmail.com>
2023-06-30 17:48:06 -0400

    Update DocString.tsx

--- src/components/DocString.tsx
+++ src/components/DocString.tsx
@@ -5,6 +5,12 @@ type DocStringProps = {
   children: string;
 };
 
+const STYLES = {
+  boxShadow: 0 3px 10px rgb(0 0 0 / 0.2),
+  whiteSpace: 'pre-line',
+  padding: '20px'
+};
+
 export default function DocString({ children }: DocStringProps) {
   const { colorMode } = useColorMode();
 
