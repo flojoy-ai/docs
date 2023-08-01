453e2d38461f2e9522fe4da66d8934df8e1d0ed6
JeffDotPng <zhangjeff2014@gmail.com>
2023-08-01 12:24:53 -0400

    Fix explore node

--- src/components/explore/nodes/DataNodeLink.tsx
+++ src/components/explore/nodes/DataNodeLink.tsx
@@ -4,7 +4,7 @@ import { NodeLinkProps, NodeLinkWrapper } from './NodeLink';
 export const DataNodeLink = ({ children, ...props }: NodeLinkProps) => {
   return (
     <NodeLinkWrapper {...props}>
-      <div className="flex min-w-fit items-center justify-center rounded-full border border-accent2 px-4 py-2 text-accent2 duration-150 hover:bg-accent2/10">
+      <div className="flex min-w-fit items-center justify-center rounded-full border border-solid border-accent2 px-4 py-2 text-accent2 duration-150 hover:bg-accent2/10">
         {children}
       </div>
     </NodeLinkWrapper>
