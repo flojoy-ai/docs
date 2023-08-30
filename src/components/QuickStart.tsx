f8ac8068d789f21b58834800e02bcdd29bc54cb9
JeffDotPng <zhangjeff2014@gmail.com>
2023-08-30 19:24:50 -0400

    responsiveness

--- src/components/QuickStart.tsx
+++ src/components/QuickStart.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import ReactSimplyCarousel from 'react-simply-carousel';
 import ThemedImage from '@theme/ThemedImage';
-import useBaseUrl from '@docusaurus/useBaseUrl';
 import { useMediaQuery } from '../hooks/useMediaQuery';
 
 type CarouselItem = {
@@ -16,7 +15,7 @@ type CarouselItem = {
   };
 };
 
-const carouselData = [
+const carouselData: CarouselItem[] = [
   {
     title: 'Predictive maintenance',
     link: 'nodes/AI_ML/PREDICT_TIME_SERIES/PROPHET_PREDICT/',
@@ -25,7 +24,8 @@ const carouselData = [
       darkMobile:
         'https://res.cloudinary.com/dhopxs1y3/image/upload/v1693367638/carousel/Predictive_Maintenance_x5gldi.png',
       dark: 'https://res.cloudinary.com/dhopxs1y3/image/upload/v1692133019/carousel/prophet_dark_jppt52.jpg',
-      lightMobile: '',
+      lightMobile:
+        'https://res.cloudinary.com/dhopxs1y3/image/upload/v1693437488/carousel/Predictive_Maintenance_u6y77e.png',
       light:
         'https://res.cloudinary.com/dhopxs1y3/image/upload/v1692133019/carousel/prophet_lite_htglx4.jpg',
     },
@@ -38,7 +38,8 @@ const carouselData = [
       darkMobile:
         'https://res.cloudinary.com/dhopxs1y3/image/upload/v1693367638/carousel/GPT_Image_Labeling_pde6q2.png',
       dark: 'https://res.cloudinary.com/dhopxs1y3/image/upload/v1692133018/carousel/nlp_gpt_dark_bvr0kk.jpg',
-      lightMobile: '',
+      lightMobile:
+        'https://res.cloudinary.com/dhopxs1y3/image/upload/v1693437488/carousel/GPT_Image_Labeling_qaci4i.png',
       light:
         'https://res.cloudinary.com/dhopxs1y3/image/upload/v1692133018/carousel/nlp_gpt_lite_l1ynxu.jpg',
     },
@@ -51,7 +52,8 @@ const carouselData = [
       darkMobile:
         'https://res.cloudinary.com/dhopxs1y3/image/upload/v1693367638/carousel/Data_Visualization_nzjrli.png',
       dark: 'https://res.cloudinary.com/dhopxs1y3/image/upload/v1692132113/carousel/3d_surf_dark_mm8igp.jpg',
-      lightMobile: '',
+      lightMobile:
+        'https://res.cloudinary.com/dhopxs1y3/image/upload/v1693437488/carousel/Data_Visualization_ghbffi.png',
       light:
         'https://res.cloudinary.com/dhopxs1y3/image/upload/v1692132113/carousel/3d_surf_lite_swvvzg.jpg',
     },
@@ -64,7 +66,8 @@ const carouselData = [
       darkMobile:
         'https://res.cloudinary.com/dhopxs1y3/image/upload/v1693367638/carousel/Signal_Processing_evavau.png',
       dark: 'https://res.cloudinary.com/dhopxs1y3/image/upload/v1692133018/carousel/fir_dark_rbfg8g.jpg',
-      lightMobile: '',
+      lightMobile:
+        'https://res.cloudinary.com/dhopxs1y3/image/upload/v1693437488/carousel/Signal_Processing_xkm1dg.png',
       light:
         'https://res.cloudinary.com/dhopxs1y3/image/upload/v1692133018/carousel/fir_lite_qad7px.jpg',
     },
@@ -77,7 +80,8 @@ const carouselData = [
       darkMobile:
         'https://res.cloudinary.com/dhopxs1y3/image/upload/v1693367638/carousel/Least_Squares_tuhow1.png',
       dark: 'https://res.cloudinary.com/dhopxs1y3/image/upload/v1692133018/carousel/regressions_dark_bo4cvx.jpg',
-      lightMobile: '',
+      lightMobile:
+        'https://res.cloudinary.com/dhopxs1y3/image/upload/v1693437488/carousel/Least_Squares_unajus.png',
       light:
         'https://res.cloudinary.com/dhopxs1y3/image/upload/v1692133019/carousel/regressions_lite_hydm25.jpg',
     },
@@ -90,7 +94,8 @@ const carouselData = [
       darkMobile:
         'https://res.cloudinary.com/dhopxs1y3/image/upload/v1693367638/carousel/One-hot_Encoding_ccisv0.png',
       dark: 'https://res.cloudinary.com/dhopxs1y3/image/upload/v1692133018/carousel/hot_encoding_dark_kjlfls.jpg',
-      lightMobile: '',
+      lightMobile:
+        'https://res.cloudinary.com/dhopxs1y3/image/upload/v1693437488/carousel/One-hot_Encoding_aqlzrv.png',
       light:
         'https://res.cloudinary.com/dhopxs1y3/image/upload/v1692133019/carousel/hot_encoding_lite_pgrbfp.jpg',
     },
@@ -99,7 +104,8 @@ const carouselData = [
 
 function Quickstart() {
   const [activeSlideIndex, setActiveSlideIndex] = useState(0);
-  const large = useMediaQuery(640);
+  const small = useMediaQuery(640);
+  const large = useMediaQuery(1024);
 
   return (
     <div>
@@ -150,19 +156,24 @@ function Quickstart() {
           },
         ]}
         speed={400}
-        easing="linear"
+        easing="ease-in-out"
       >
         {carouselData.map(item => (
-          <div style={{ width: large ? 730 : 256, height: large ? 330 : 540 }}>
+          <div
+            style={{
+              width: large ? 730 : small ? 540 : 256,
+              height: large ? 330 : small ? 250 : 540,
+            }}
+          >
             <a href={item.link}>
-              {large && (
+              {small && (
                 <h2 className="absolute text-lg sm:text-xl">{item.title}</h2>
               )}
               <ThemedImage
                 alt={item.image.alt}
                 sources={{
-                  light: large ? item.image.light : item.image.lightMobile,
-                  dark: large ? item.image.dark : item.image.darkMobile,
+                  light: small ? item.image.light : item.image.lightMobile,
+                  dark: small ? item.image.dark : item.image.darkMobile,
                 }}
               />
             </a>
