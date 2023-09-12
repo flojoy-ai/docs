6e3c16b22d3816dc5907917c647129aab677f2e0
Jack Parmer <jparmer09@gmail.com>
2023-09-11 23:15:12 -0400

    use AppThumbnail component for instrument thumbnails

--- src/components/AppThumbnail.tsx
+++ src/components/AppThumbnail.tsx
@@ -5,13 +5,23 @@ import ScipyNode from '@site/src/components/nodes/ScipyNode';
 
 type AppThumbnailProps = {
   children: string;
+  path: string;
+  img: string;
+  instrument: boolean;
 };
 
-export default function AppThumbnail({ children, path }: AppThumbnailProps) {
+export default function AppThumbnail({ children, path, img, instrument }: AppThumbnailProps) {
+
   const { colorMode } = useColorMode();
-  let thumbnailPath = 'examples/EX1/output.jpeg';
-  let nodeLabel = path.split('/').pop();
+  let thumbnailPath:string = 'examples/EX1/output.jpeg';
+  let nodeLabel:string = path.split('/').pop();
   let nodeData = {label: nodeLabel}
+  
+  const imgSrc = typeof img === 'string' ? img : `https://raw.githubusercontent.com/flojoy-ai/docs/main/docs/nodes/${path}/${thumbnailPath}`;
+
+  const thumbnailClass = instrument ? 'instrument-thumbnail-figure' : 'app-thumbnail-figure';
+
+  const pathRoot = instrument ? 'instruments-database' : 'nodes';
 
   if( path.toLowerCase().indexOf('numpy') >= 0 ) {
     return (
@@ -35,13 +45,15 @@ export default function AppThumbnail({ children, path }: AppThumbnailProps) {
 
   return (
     <div className="p-4">
-        <a href={`/nodes/${path}/`}>
+        <a href={`/${pathRoot}/${path}/`}>
             {
-            <figure className="app-example-thumbnail">
-              <img 
-                alt={children}
-                src={`https://raw.githubusercontent.com/flojoy-ai/docs/main/docs/nodes/${path}/${thumbnailPath}`}
-              />
+            <figure className={`category-page-thumbnail ${thumbnailClass}`}>
+              <div class='thumbnail-image-container'>
+                <img 
+                  alt={children}
+                  src={imgSrc}
+                />
+              </div>
               <figcaption>{children}</figcaption>
             </figure>
             }
