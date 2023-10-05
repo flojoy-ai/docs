44bd5a813c3a58687c42d648489bfb22856f4ccc
Joey Yu <joey@jyu.dev>
2023-10-04 20:26:34 -0400

    Small hover animation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,7 +15,7 @@ export default function Card({
 }: CardProps) {
   return (
     <a
-      className="flex flex-col gap-2 rounded-2xl border-4 border-modal p-8 text-black hover:no-underline dark:text-white"
+      className="flex flex-col gap-2 rounded-2xl border-4 border-modal p-8 text-black transition duration-300 hover:bg-accent2/10 hover:no-underline dark:text-white"
       href={`${cardLink}`}
     >
       <div className="flex gap-2 text-2xl font-bold">
