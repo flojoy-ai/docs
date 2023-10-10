e5fbdca5058f6fed7371f08065e607dee441fa2d
Jack Parmer <jparmer09@gmail.com>
2023-10-10 11:49:38 -0400

    more css fixups

--- src/components/UniversalAdmonition.tsx
+++ src/components/UniversalAdmonition.tsx
@@ -6,7 +6,7 @@ const UniversalAdmonition = () => {
             <div className="admonitionHeading_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-styles-module">
                 <span className="admonitionIcon_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-styles-module">
                     <svg viewBox="0 0 14 16"
-                        style={{maxWidth: '15px', marginRight: '4px', verticalAlign: 'sub'}}
+                        style={{maxWidth: '15px', marginRight: '4px', verticalAlign: 'sub', fill: 'white', marginBottom: '1px'}}
                     >
                         <path fill-rule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path>
                     </svg>
@@ -14,7 +14,10 @@ const UniversalAdmonition = () => {
             </div>
             <div className="admonitionContent_node_modules-@docusaurus-theme-classic-lib-theme-Admonition-styles-module">
                 <details 
-                    className="details_node_modules-@docusaurus-theme-common-lib-components-Details-styles-module isBrowser_node_modules-@docusaurus-theme-common-lib-components-Details-styles-module alert alert--info details_node_modules-@docusaurus-theme-classic-lib-theme-Details-styles-module" data-collapsed="true">
+                    className="details_node_modules-@docusaurus-theme-common-lib-components-Details-styles-module isBrowser_node_modules-@docusaurus-theme-common-lib-components-Details-styles-module alert alert--info details_node_modules-@docusaurus-theme-classic-lib-theme-Details-styles-module"
+                    style={{border: '1px solid', padding: '0.5rem', fontSize: '80%', cursor: 'pointer'}}
+                    data-collapsed="true"
+                >
                     <summary><i>Flojoy is a free, open-source replacement for engineering software such as LabVIEW and Simulink.</i></summary>
                     <div>
                         <div className="collapsibleContent_node_modules-@docusaurus-theme-common-lib-components-Details-styles-module">
